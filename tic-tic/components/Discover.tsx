275ff174f079965096b6c0435354e8825dfad12b
ozzkan-burak <brk.ozzkan@gmail.com>
2023-01-07 23:19:19 +0300

    episode23

--- tic-tic/components/Discover.tsx
+++ tic-tic/components/Discover.tsx
@@ -1,9 +1,19 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Discover = () => {
   return (
-    <div>
-      Discover
+    <div className="xl:border-b-2 xl:border-gray-200 pb-6">
+      <p className="text-gray-500 font-semibold m-3 mt-4 hidden xl:bloxk">
+        Popular Topics
+      </p>
+      <div className="flex gap-3 flex-wrap">
+        {
+          topics.map((topic)=>(
+            <Link href={`/?topic=${topic.name}`}></Link>
+          ))
+        }
+      </div>
     </div>
   )
 }
