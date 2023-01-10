6765b3a93459bb8ee75d633ae574964d46611bae
ozzkan-burak <brk.ozzkan@gmail.com>
2023-01-10 23:59:11 +0300

    episode26

--- tic-tic/components/Discover.tsx
+++ tic-tic/components/Discover.tsx
@@ -6,9 +6,13 @@ import { topics } from '../utils/constants';
 
 const Discover = () => {
 
-  const activeTopicStyle = "";
+  const router = useRouter();
+  const { topic } = router.query;
+  console.log(router)
 
-  const topicStyle = "xl:border-2 hover:bg-primary vl: border-gray-300 px-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-black"
+  const activeTopicStyle = "xl:border-2 hover:bg-primary xl:border-[#F51997] px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-[#FF1997]";
+
+  const topicStyle = "xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-black"
 
   return (
     <div className="xl:border-b-2 xl:border-gray-200 pb-6">
@@ -17,14 +21,14 @@ const Discover = () => {
       </p>
       <div className="flex gap-3 flex-wrap">
         {
-          topics.map((topic) => (
-            <Link key={topic.name} href={`/?topic=${topic.name}`}>
-              <div className={topicStyle}>
+          topics.map((item) => (
+            <Link key={item.name} href={`/?topic=${item.name}`}>
+              <div className={topic === item.name ? activeTopicStyle : topicStyle}>
                 <span className="font-bold text-2x xl:text-md">
-                  {topic.icon}
+                  {item.icon}
                 </span>
                 <span className="font-medium text-md hidden xl:block capitalize">
-                  {topic.name}
+                  {item.name}
                 </span>
               </div>
             </Link>
