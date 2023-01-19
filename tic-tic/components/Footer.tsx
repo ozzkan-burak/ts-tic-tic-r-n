af6f972d467ab200c056b12d8d1e2076ba3ed7fc
ozzkan-burak <brk.ozzkan@gmail.com>
2023-01-19 23:52:27 +0300

    episode26

--- tic-tic/components/Footer.tsx
+++ tic-tic/components/Footer.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+
+import { footerList1, footerList2, footerList3 } from '../utils/constants';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="mt-6 hidden xl:block">
+      <div className="flex flex-wrap gap-2 mt-5">
+        {footerList1.map((item, index)=> (
+          <p key={`item-${index.toString()}`} className="text-gray-400 text-sm hover:underline cursor-pointer">
+            {item}
+          </p>
+        ))}
+      </div>
+    </div>
   )
 }
 
