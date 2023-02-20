5c134de05b353372961af20b38135ca68d0ac377
ozzkan-burak <brk.ozzkan@gmail.com>
2023-02-20 19:00:46 +0300

    episode28

--- tic-tic/pages/index.tsx
+++ tic-tic/pages/index.tsx
@@ -1,11 +1,19 @@
+import { NextPage } from 'next';
+import axios from 'axios';
 import Head from 'next/head'
 import Image from 'next/image'
 
 
-export default function Home() {
+const Home: NextPage = () => {
   return (
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
   )
 }
+
+export const getServerSideProps = async () => {
+  const response = await axios.get();
+};
+
+export default Home;
